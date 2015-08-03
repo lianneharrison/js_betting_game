b5e1502fbea1711ffdd1f2a641cdc1f3477f85a0
Lianne <lyharrison@sky.com>
2015-08-03 18:14:36 +0000

    part 1 of exercise complete. just need to add in a loop

---
+++ js_betting_game.js
@@ -0,0 +1,64 @@
+// var funds = 100
+// var bet = prompt("How much do you want to bet")
+// var answer = prompt("Pick a number between 1 and 10")
+
+// var random = Math.floor((Math.random() * 10) + 1);
+
+// if answer === random
+//   console.log("You win!");
+//   funds += bet * 2
+
+// else if answer === random + 1 || random - 1
+//   console.log("Nearly!");
+//   funds += bet
+
+// else 
+//   console.log("You lose!");
+//   funds -= bet
+  
+  // var funds = 100;
+  // var bet = prompt("How much do you want to bet");
+  // var answer = prompt("Pick a number between 1 and 10");
+  // var random = Math.floor((Math.random() * 10) + 1);
+  // if (answer === random){
+  //   alert("Well done!");
+  //   funds += bet * 2
+  // }
+  // else if (answer === random + 1 || random - 1){
+  //   alert("Nearly!");
+  //   funds += bet
+  // }
+  // else {
+  //   alert("You lose!");
+  //   funds -= bet
+  // }
+  //psuedocode above
+
+
+function bettingGame() {
+  var funds = 100;
+  var bet = prompt("How much do you want to bet, $5 or $10?");
+  var answer = parseInt(prompt("Pick a number between 1 and 10"));
+  var random = Math.floor((Math.random() * 10) + 1);
+  if (answer === random){
+    console.log(random);
+    alert("Well done!");
+    funds += bet * 2;
+    console.log(funds)
+  }
+  else if (answer === random + 1 || answer === random - 1){
+    console.log(random);
+    alert("Nearly!");
+    alert(funds)
+  }
+  else {
+    console.log(random);
+    alert("You lose!");
+    funds -= bet;
+    alert(funds)
+  }
+}
+
+
+
+
